59880799b9b26f6c0169f34be6a3875a1ad79484
Tarrell Fletcher <Tarrell13@verizon.net>
2016-12-20 00:02:39 -0500

    Modifications Everywhere

--- recycle.py
+++ recycle.py
@@ -12,7 +12,7 @@ Options:
 -v:             Verbose Output
 -h, --help:     Show help menu with commands
 --show-bin:     Show trash bin contents
---empty:        Empty trash contents
+--nuke:         Empty trash contents
 
 
 Example: (1) ./recycle boo.txt
@@ -29,10 +29,10 @@ import sys
 def usage():
     print("Usage: ./recycle OPTIONS <Files or Directory>")
     print("Options: ")
-    print(" -v:         Verbose Output")
+    print("-v:         Verbose Output")
     print("-h, --help:  Show help menu")
     print("--show-bin:  Show trash bin contents(RUN ALONE)")
-    print("--empty:     Empty trash bin contents(RUN ALONE)")
+    print("--nuke:     Empty trash bin contents(RUN ALONE)")
     print("")
     print("Example: (1) ./recycle boo.txt")
     print("---------(2) ./recycle boo.txt foo.txt")
@@ -40,36 +40,70 @@ def usage():
     print("---------(4) ./recycle *.txt")
     sys.exit()
 
-commands = ["-v", "-h", "--help", "--show-bin", "--empty"]
+
+'''Commands'''
+commands = ["-v", "-h", "--help", "--show-bin", "--nuke"]
 
 VERBOSE = False
 MAC = False
 LINUX = False
+NUKE = False
+SHOWBIN = False
+
+
+
+
+
 
 '''Checks to Determine Operating System'''
 def systemCheck():
+    global MAC, LINUX
+
     if os.uname()[0] == "Darwin":
         MAC = True
     elif os.uname()[0] == "Linux":
         LINUX = True
+    else:
+        print("System Unknown")
+
+
+
+
+
 
 '''Turns command switches on'''
 def switchOn(arguments):
+    global VERBOSE, NUKE, SHOWBIN
+
     for i in range(1, len(arguments)):
         if arguments[i] in commands:
             if str(arguments[i]) == "-v":
                 VERBOSE = True
+            elif str(arguments[i]) in commands == "--nuke":
+                NUKE = True
+            elif str(arguments[i]) in commands == "--show-bin":
+                showBin()
+            elif str(arguments[i]) == "-h" or str(arguments[i]) == "--help":
+                usage()
 
 
+
+
+
+
+'''For verbose output'''
 def success(file):
-
     location = '"'
 
-    if os.uname()[0] == "Darwin":
+    if MAC:
         location = "~/.Trash"
-    elif os.uname()[0] == "Linux":
+    elif LINUX:
         location = "~/.local/share/Trash"
     return "\"%s\" successfully sent to %s" %(file, location)
+
+
+
+
 
 '''Handle Multiple Command Line Files'''
 def multi_line(arguments):
@@ -88,13 +122,20 @@ def multi_line(arguments):
             print("\"%s\" does not exist" %arguments[i])
 
 
+
+
+
+
 '''Some commands can only be run if they are Specified Alone!'''
 def sanity(arguments):
     if len(arguments) == 2:
         return True
     else:
-        print("!!!Command:\"%s\" must be run alone!!!")
-        sys.exit()
+        return False
+
+
+
+
 
 
 '''Show trash bin contents'''
@@ -112,11 +153,53 @@ def showBin():
             print("[+] %s" %file)
     else:
         print("Error")
+    sys.exit()
 
 
-#multi_line(sys.argv)
 
-systemCheck()
-showBin()
+
+
+'''Nuke the Trash Bin'''
+def nuke():
+    if MAC:
+        os.system("rm -rf " + os.environ["HOME"] + "/.Trash/*")
+    elif LINUX:
+        os.system("rm -rf " + os.environ["HOME"] + "/.usr/local/files/*")
+        os.system("rm -rf " + os.environ["HOME"] + "/.usr/local/info/*")
+
+    print("Trash Bin Emptied")
+    sys.exit()
+
+
+
+
+
+
+def main():
+
+    systemCheck()
+    switchOn(sys.argv)
+
+    '''Displays help menu'''
+    if len(sys.argv) < 2:
+        usage()
+
+    '''Checks for too many arguments when nuke or show-nin'''
+    if len(sys.argv) >= 3:
+        if NUKE:
+            print("!!! Nuke must be run alone !!!")
+    else:
+        if sanity(sys.argv):
+            if NUKE:
+                nuke()
+
+        multi_line(sys.argv)
+
+
+
+main()
 sys.exit()
+
+
+
 
